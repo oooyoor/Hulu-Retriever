668622bcb6022f30ea63bae86818c708b4d745d0
hululuzz <hululuz@yeah.net>
2025-11-22 07:59:41 +0000

    fix-init-submodule

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-if [ ! -d "external" ]; then
-    mkdir external
+set -e
+
+EXTERNAL_DIR="external"
+mkdir -p "$EXTERNAL_DIR"
+if ! git config --file .gitmodules --name-only --get-regexp "submodule.external/hnswlib.path" >/dev/null 2>&1; then
+    git submodule add git@github.com:nmslib/hnswlib.git external/hnswlib
 fi
-cd external
-if [ ! -d "hnswlib" ]; then
-    git submodule add git@github.com:nmslib/hnswlib.git
+if ! git config --file .gitmodules --name-only --get-regexp "submodule.external/spdlog.path" >/dev/null 2>&1; then
+    git submodule add git@github.com:gabime/spdlog.git external/spdlog
 fi
-if [ ! -d "spdlog" ]; then
-    git submodule add git@github.com:gabime/spdlog.git
-fi
-if [ ! -f "json.hpp" ]; then    
-    wget https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
+git submodule update --init --recursive
+JSON_PATH="$EXTERNAL_DIR/json.hpp"
+if [ ! -f "$JSON_PATH" ]; then
+    wget https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp -O "$JSON_PATH"
 fi
